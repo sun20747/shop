0c3eba7604af29bcba0d600c698620f69b609e30
sun20747 <sunny20747@gmail.com>
2021-09-19 11:59:34 +0700

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="flex space-x-4">
+      <div class="flex-1">
+        <div class="flex items-center border-b border-teal-500 py-2 px-10">
+          <input
+            class="
+              appearance-none
+              bg-transparent
+              border-none
+              w-full
+              text-gray-700
+              mr-3
+              py-1
+              px-2
+              leading-tight
+              focus:outline-none
+            "
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+      <div class="flex-1 text-center text-3xl">adalene.</div>
+      <div class="flex-1">3</div>
+    </div>
+
+    <div class="flex space-x-4">
+      <div class="flex-1">1</div>
+      <div class="flex-1">2</div>
+      <div class="flex-1">3</div>
+    </div>
+  </div>
+</template>
